86b6a6053217c628fbaa9b9b78c51fe43cb6b6fe
guoguolan1991 <guoguolan1991@gmail.com>
2017-08-04 16:46:31 +0800

    add hanlp first demo

---
+++ learn/src/main/java/learn/LearnHanLP.java
@@ -0,0 +1,13 @@
+package learn;
+
+import com.hankcs.hanlp.HanLP;
+
+/**
+ * Created by Miller on 2017/8/4 0004.
+ */
+public class LearnHanLP {
+    public static void main(String[] args){
+        System.out.println(HanLP.segment("开始hanlp分词尝试"));
+    }
+
+}
